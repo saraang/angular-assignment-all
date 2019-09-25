1d0ff9b30adeb3c07113f0275a15f68e31b3a227
saraang <saraangsk@gmail.com>
2019-09-25 10:50:25 +0530

    Implementation of two way binding started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Assignment1Component } from './assignment1/assignment1.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, Assignment1Component ],
+  declarations: [ AppComponent, Assignment1Component, Assignment2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
